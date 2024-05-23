eecb8ee3ba44ffba1981411af7411776c0c1b5cf
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-05-23 15:09:18 +0000

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,17 +1,8 @@
-sudo apt update
-sudo apt install nala -y
-sudo nala upgrade -y
-sudo nala install htop -y
-sudo nala install vlc -y
-sudo nala install blender -y
-sudo nala purge firefox -y
-sudo nala install -y wget gnupg lsb-release apt-transport-https ca-certificates
+# Adding Nala and Items Needed For Adding Repositories
+sudo apt update && sudo apt install nala -y && sudo nala upgrade -y && sudo nala install -y wget gnupg lsb-release apt-transport-https ca-certificates
 
-distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
-
-wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-
-sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null 
+# Additional Repositories Added
+distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi) && wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg && sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
 Types: deb
 URIs: https://deb.librewolf.net
 Suites: $distro
@@ -19,28 +10,13 @@ Components: main
 Architectures: amd64
 Signed-By: /usr/share/keyrings/librewolf.gpg
 EOF
+sudo add-apt-repository ppa:phoerious/keepassxc & flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-sudo nala update
-sudo nala install librewolf -y
-sudo nala install flatpak -y
-sudo nala install plasma-discover-backend-flatpak -y
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub io.freetubeapp.FreeTube -y
-sudo add-apt-repository ppa:phoerious/keepassxc
-sudo nala update
-sudo nala install keepassxc -y
-sudo nala install neovim -y
-flatpak install flathub com.usebottles.bottles -y
-
-
-
-
-
-
-
-
-
-
+# Installing Apps 
+#  Via Nala - (Blender, VLC, Htop, Flatpak, Plasma Discover Flatpak, Neovim, Librewolf, Keepassxc) 
+#  Via Flatpak - (Freetube, Bottles) 
+#  Removes - (Firefox) 
+sudo nala upgrade -y && sudo nala install -y blender vlc htop flatpak plasma-discover-backend-flatpak neovim librewolf keepassxc & sudo nala purge firefox -y && flatpak install flathub io.freetubeapp.FreeTube -y && flatpak install flathub com.usebottles.bottles -y
 
 
 
