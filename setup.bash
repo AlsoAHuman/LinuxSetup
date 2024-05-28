292dcd063a05b8522ee7a6da567702e73d848970
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-05-28 11:49:57 -0400

    Update setup.bash
    adds sudo requirement

--- setup.bash
+++ setup.bash
@@ -1,3 +1,9 @@
+# Check if Script is Run as Root
+    if [[ $EUID -ne 0 ]]; then
+      echo "You must be a root user to run this script, please run sudo ./setup.sh" 2>&1
+      exit 1
+    fi
+
 # Adding Nala and Items Needed For Adding Repositories
     sudo apt update
     sudo apt install nala -y
