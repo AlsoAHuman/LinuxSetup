32392c9a7bac3287d2b31dfb11997e7034527e18
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-06-02 20:32:53 +0000

    Update setup.bash
    added sudo to platpak scripts. moved podman from flatpak to nala type install

--- setup.bash
+++ setup.bash
@@ -45,14 +45,13 @@
 # Installing Apps 
     sudo nala upgrade -y
     
-    # Via Nala - (Blender, VLC, Htop, Flatpak, Plasma Discover Flatpak, Neovim, Librewolf, Keepassxc, Proton VPN, Ranger, ADB, Curl, Codium) 
-        sudo nala install blender vlc htop flatpak plasma-discover-backend-flatpak neovim librewolf keepassxc proton-vpn-gnome-desktop ranger adb curl codium -y
+    # Via Nala - (Blender, VLC, Htop, Flatpak, Plasma Discover Flatpak, Neovim, Librewolf, Keepassxc, Proton VPN, Ranger, ADB, Curl, Codium, Podman GUI) 
+        sudo nala install blender vlc htop flatpak plasma-discover-backend-flatpak neovim librewolf keepassxc proton-vpn-gnome-desktop ranger adb curl codium podman -y
    
-    # Via Flatpak - (Freetube, Bottles, GIMP, Podman GUI) 
-        flatpak install flathub io.freetubeapp.FreeTube -y
-        flatpak install flathub com.usebottles.bottles -y
-        flatpak install flathub org.gimp.GIMP -y
-        flatpak install flathub io.podman_desktop.PodmanDesktop -y
+    # Via Flatpak - (Freetube, Bottles, GIMP) 
+        sudo flatpak install flathub io.freetubeapp.FreeTube -y
+        sudo flatpak install flathub com.usebottles.bottles -y
+        sudo flatpak install flathub org.gimp.GIMP -y
 
     # Via Curl - (Bun)
         curl -fsSL https://bun.sh/install | bash
