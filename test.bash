24f3c81b71cc9be810466fb4b25f9acd123e11ea
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-06-25 01:02:14 +0000

    Delete test.bash

--- test.bash
+++
@@ -1,5 +0,0 @@
-sudo apt update
-    sudo apt install nala -y
-    sudo nala fetch --auto --fetches 4 -y
-    sudo nala upgrade -y
-   # sudo nala install blender vlc htop flatpak plasma-discover-backend-flatpak neovim librewolf keepassxc proton-vpn-gnome-desktop ranger adb curl codium podman libreoffice kdenlive ffmpeg libsdl2-2.0-0 adb gcc pkg-config meson ninja-build libsdl2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libusb-1.0-0 libusb-1.0-0-dev -y
