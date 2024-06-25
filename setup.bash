4faba0f1d52ff5e6c9c05fb7eeb105c6bf16b738
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-06-25 01:06:08 +0000

    Update setup.bash
    break up added dependency install for nala because if pkg isnt found it wont install everything

--- setup.bash
+++ setup.bash
@@ -33,8 +33,9 @@
     sudo nala upgrade -y
     
     # Via Nala - (Blender, VLC, Htop, Flatpak, Plasma Discover Flatpak, Neovim, Librewolf, Keepassxc, Proton VPN, Ranger, ADB, Curl, Codium, Podman, Libreoffice, Kdenlive, ffempeg, ADB) 
-        sudo nala install blender vlc htop flatpak plasma-discover-backend-flatpak neovim librewolf keepassxc proton-vpn-gnome-desktop ranger adb curl codium podman libreoffice kdenlive ffmpeg libsdl2-2.0-0 adb gcc pkg-config meson ninja-build libsdl2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libusb-1.0-0 libusb-1.0-0-dev -y
-   
+        sudo nala install blender vlc htop flatpak plasma-discover-backend-flatpak neovim keepassxc ranger adb curl podman libreoffice kdenlive ffmpeg libsdl2-2.0-0 adb gcc pkg-config meson ninja-build libsdl2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libusb-1.0-0 libusb-1.0-0-dev -y
+       sudo nala install codium librewolf proton-vpn-gnome-desktop -y
+       
     # Via Flatpak - (Freetube, Bottles, GIMP, Podman Desktop) 
         flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
         flatpak install flathub io.freetubeapp.FreeTube -y
