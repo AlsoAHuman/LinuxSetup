8032ec0fadec50a98dd5d282237231c4934deb54
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-06-26 22:20:33 +0000

    test mrk4

--- test.bash
+++ test.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
     sudo apt install curl -y
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh | yes -s "1"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -y | sh
