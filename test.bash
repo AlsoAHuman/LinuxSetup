421231ea59d7da12cb150d208f4c02b269b5a87a
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-06-26 22:41:15 +0000

    test mrk6

--- test.bash
+++ test.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
     sudo apt install curl -y
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+    echo "1" | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
