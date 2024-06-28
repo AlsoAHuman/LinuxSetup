9b0c187c12d8d39b1c6e2724b1d9f55f19add1b2
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-06-28 16:44:48 +0000

    formatting AlsoAHuman/My-Linux-Setup/Repos_List/LibrewolfRepo.bash and added test.bash

--- Repos_List/LibrewolfRepo.bash
+++ Repos_List/LibrewolfRepo.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Adds Librewolf Repository
-        distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
-        wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-        sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
-        Types: deb
-        URIs: https://deb.librewolf.net
-        Suites: $distro
-        Components: main
-        Architectures: amd64
-        Signed-By: /usr/share/keyrings/librewolf.gpg
-        EOF 
+        # Adds Librewolf Repository
+                distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
+                wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+                sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+                Types: deb
+                URIs: https://deb.librewolf.net
+                Suites: $distro
+                Components: main
+                Architectures: amd64
+                Signed-By: /usr/share/keyrings/librewolf.gpg
+                EOF 
