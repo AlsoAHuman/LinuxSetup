e7902d601699b953fde1bb2b04db530cc0ff4654
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-06-28 17:13:44 +0000

    remove cd from test.bash and librewolfrepo.bash

--- Repos_List/LibrewolfRepo.bash
+++ Repos_List/LibrewolfRepo.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 
         # Adds Librewolf Repository
-                cd 
                 distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
                 wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
                 sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
