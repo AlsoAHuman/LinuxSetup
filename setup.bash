ebc9c435954ec38f5893659e5e15b362b464cf11
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-06-30 17:54:02 +0000

    librewolf test

--- setup.bash
+++ setup.bash
@@ -20,7 +20,18 @@
     # Additional Repositories Added
 
         # Librewolf Repo
-            sudo bash ./Repos_List/LibrewolfRepo.bash
+            distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
+
+            wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+
+            sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+            Types: deb
+            URIs: https://deb.librewolf.net
+            Suites: $distro
+            Components: main
+            Architectures: amd64
+            Signed-By: /usr/share/keyrings/librewolf.gpg
+            EOF
 
         # Protonvpn Repo
             wget https://repo2.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb
@@ -36,11 +47,11 @@
     # Installing Apps 
         sudo nala upgrade -y
 
-        # Via Nala - (Blender, VLC, Htop, Flatpak, Plasma Discover Flatpak, Neovim, Librewolf, Keepassxc, Proton VPN, Ranger, ADB, Curl, Codium, Podman, Libreoffice, Kdenlive, ffempeg, ADB) 
-            sudo nala install blender vlc htop flatpak plasma-discover-backend-flatpak neovim keepassxc ranger adb curl podman libreoffice kdenlive ffmpeg libsdl2-2.0-0 adb gcc pkg-config meson ninja-build libsdl2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libusb-1.0-0 libusb-1.0-0-dev -y
+        # Via Nala - (Blender, VLC, Htop, Flatpak, Plasma Discover Flatpak, Neovim, Librewolf, Keepassxc, Proton VPN, Ranger, ADB, Curl, Codium, Podman, Libreoffice, Kdenlive, ffempeg) 
+            sudo nala install blender vlc htop flatpak plasma-discover-backend-flatpak neovim keepassxc ranger adb curl podman libreoffice kdenlive ffmpeg libsdl2-2.0-0 adb gcc pkg-config meson ninja-build libsdl2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libusb-1.0-0 libusb-1.0-0-dev librewolf -y
             
-        # Via Internal Script Execution - (Librewolf, Codium, Proton-Vpn-Gnome-Desktop)
-            sudo nala install codium librewolf proton-vpn-gnome-desktop -y
+        # Via Internal Script Execution - (Codium, Proton-Vpn-Gnome-Desktop)
+            sudo nala install codium proton-vpn-gnome-desktop -y
 
         # Via Flatpak - (Freetube, Bottles, GIMP, Podman Desktop) 
             flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
