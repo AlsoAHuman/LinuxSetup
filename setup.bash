d125dac0dac8d44252b3112c0fe0e6e4f4a9969b
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-07-01 13:56:17 +0000

    librewolf possible final

--- setup.bash
+++ setup.bash
@@ -19,20 +19,6 @@
 
     # Additional Repositories Added
 
-        # Librewolf Repo
-            distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
-
-            wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-
-            sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
-            Types: deb
-            URIs: https://deb.librewolf.net
-            Suites: $distro
-            Components: main
-            Architectures: amd64
-            Signed-By: /usr/share/keyrings/librewolf.gpg
-            EOF
-
         # Protonvpn Repo
             wget https://repo2.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-3_all.deb
             sudo dpkg -i ./protonvpn-stable-release_1.0.3-3_all.deb
@@ -47,7 +33,7 @@
     # Installing Apps 
         sudo nala upgrade -y
 
-        # Via Nala - (Blender, VLC, Htop, Flatpak, Plasma Discover Flatpak, Neovim, Librewolf, Keepassxc, Proton VPN, Ranger, ADB, Curl, Codium, Podman, Libreoffice, Kdenlive, ffempeg) 
+        # Via Nala - (Blender, VLC, Htop, Flatpak, Plasma Discover Flatpak, Neovim, Keepassxc, Proton VPN, Ranger, ADB, Curl, Codium, Podman, Libreoffice, Kdenlive, ffempeg) 
             sudo nala install blender vlc htop flatpak plasma-discover-backend-flatpak neovim keepassxc ranger adb curl podman libreoffice kdenlive ffmpeg libsdl2-2.0-0 adb gcc pkg-config meson ninja-build libsdl2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libusb-1.0-0 libusb-1.0-0-dev librewolf -y
             
         # Via Internal Script Execution - (Codium, Proton-Vpn-Gnome-Desktop)
@@ -60,11 +46,12 @@
             flatpak install flathub org.gimp.GIMP -y
             flatpak install flathub io.podman_desktop.PodmanDesktop -y
 
-        # Via Git Clone - (Scrcpy)
+        # Via Git Clone - (Scrcpy, Librewolf)
             git clone https://github.com/Genymobile/scrcpy
             cd scrcpy
             ./install_release.sh
             cd
+            sudo bash Scripts_List/Librewolf.bash
 
         # Via Curl - (Bun, Rust)
             curl -fsSL https://bun.sh/install | bash
