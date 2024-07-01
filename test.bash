b1478fe37b6dd974ce91d0883953a4dae7470ce4
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-07-01 14:26:13 +0000

    delete test.bash

--- test.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-    sudo apt update
-    sudo apt install nala -y
-    sudo nala install wget gnupg lsb-release apt-transport-https ca-certificates -y
-    sudo bash ./Repos_List/LibrewolfRepo.bash
-    sudo nala update
-    sudo nala install librewolf -y
