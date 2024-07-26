4dc964c851f5a8f62c867d4c6ab23b6856e246ae
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2024-07-26 22:31:00 +0000

    Update setup.bash
    added option to install discord via y/n prompt

--- setup.bash
+++ setup.bash
@@ -6,6 +6,17 @@
           exit 1
         fi
 
+    # Discord Install Function
+        install_discord() {
+        echo "Installing Discord..."
+        wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+        sudo dpkg -i discord.deb
+        rm discord.deb
+        }
+
+    # Ask User If They Want to Install Discord
+        read -p "Do you want to install Discord? (y/n): " response
+    
     # Variables - (Username, Directory)
         username=$(id -u -n 1000)
         builddir=$(pwd)
@@ -46,6 +57,11 @@
     # Installing Apps 
         sudo nala upgrade -y
 
+        # Installing Discord (if user chose yes)
+            if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+                install_discord
+            fi
+        
         # Via Nala - (Blender, VLC, Flatpak, Signal, Keepassxc, Proton VPN, Ranger, ADB, VSCodium, Podman, Libreoffice, Kdenlive, ffempeg) 
             sudo nala install blender steam-installer mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 vlc htop flatpak plasma-discover-backend-flatpak neovim keepassxc ranger fzf adb podman libreoffice kdenlive ffmpeg libsdl2-2.0-0 bat gcc pkg-config meson ninja-build libsdl2-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libswresample-dev libusb-1.0-0 libusb-1.0-0-dev signal-desktop -y
             
