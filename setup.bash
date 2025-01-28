9bb526126877e5ee6a518217e70abf52ea0b327a
AlsoAHuman <168640526+AlsoAHuman@users.noreply.github.com>
2025-01-28 12:02:34 -0500

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -107,20 +107,19 @@
     
     # Scripts Setup
         # FastFetch
-            # Check the Debian version and install Fast Fetch
-                DEBIAN_VERSION=$(lsb_release -r -s)
-                    if [[ $DEBIAN_VERSION < 13 ]]; then
-                        
-                        # Download the latest fastfetch deb file
-                            FASTFETCH_URL=$(curl -s https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest | grep "browser_download_url.*linux-amd64.deb" | cut -d '"' -f 4)
-                            curl -sL $FASTFETCH_URL -o /tmp/fastfetch_latest_amd64.deb
-                        
-                        # Install the downloaded deb file using apt-get
-                            sudo nala install /tmp/fastfetch_latest_amd64.deb
-                    else
-                        # If Debian version is 13 or higher, use nala to install fastfetch
-                            sudo nala install fastfetch
-                    fi
+            DEBIAN_VERSION=$(lsb_release -r -s)
+                if [[ $DEBIAN_VERSION < 13 ]]; then
+                    
+                    # Download the latest fastfetch deb file
+                        FASTFETCH_URL=$(curl -s https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest | grep "browser_download_url.*linux-amd64.deb" | cut -d '"' -f 4)
+                        curl -sL $FASTFETCH_URL -o /tmp/fastfetch_latest_amd64.deb
+                    
+                    # Install the downloaded deb file using apt-get
+                        sudo nala install /tmp/fastfetch_latest_amd64.deb
+                else
+                    # If Debian version is 13 or higher, use nala to install fastfetch
+                        sudo nala install fastfetch
+                fi
 
         sudo bash Scripts_List/usenala.bash
         sudo bash Scripts_List/AutoUpdate.bash
